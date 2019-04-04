328bc3dd388949053919732e10aede637fdbc12f
Dejan Stjepanovic <Dejan.Stjepanovic@ey.com>
2019-04-04 16:47:28 -0500

    Almost finished warrior class. Will add Chain constructors tomorrow and then build up all character classes

--- MagicDestroyers/Characters/Melee/Warrior.cs
+++ MagicDestroyers/Characters/Melee/Warrior.cs
@@ -14,12 +14,117 @@ namespace MagicDestroyers.Characters.Melee
         private string name;
         private Chainlink bodyArmor;
         private Axe weapon;
-        
-        public int AblilityPoints { get; set; }
-        public string Faction { get; set; }
-        public int HealthPoints { get; set; }
-        public int Level { get; set; }
-        public string Name { get; set; }
+
+        //Ability points properties
+        public int AblilityPoints
+        {
+            get
+            {
+                return this.abilityPoints;
+            }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                {
+                    this.abilityPoints = value;
+                }
+                else throw new ArgumentOutOfRangeException(string.Empty, "Inappropriate value, the value should be >= 0 and <= 10.");
+            }
+        }
+
+        //Faction properties
+        public string Faction
+        {
+            get
+            {
+                return faction;
+            }
+            set
+            {
+                if (value == "Melee" || value == "Spellcaster")
+                {
+                    faction = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "the faction should be either Melee or Spellcaster");
+                        }
+            }
+        }
+
+        //HealthPoints properties
+        public int HealthPoints
+        {
+            get
+            {
+                return this.healthPoints;
+            }
+            set
+            {
+                if (value >= 0 && value <= 20)
+                {
+                    this.healthPoints = value;
+                }
+            else throw new ArgumentOutOfRangeException(string.Empty, "Inappropriate value, the value should be >= 0 and <= 20.");
+            }
+    }
+
+        //Level Properties
+        public int Level
+        {
+            get
+            {
+                return this.level;
+            }
+            set
+            {
+                if (value >= 1 && value <= 25)
+                {
+                    this.level = value;
+                }
+                else throw new ArgumentOutOfRangeException(string.Empty, "Inappropriate value, the value should be >= 0 and <= 25.");
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (value.Length >= 3 && value.Length <= 20)
+                {
+                    name = value;
+                }
+                else throw new ArgumentOutOfRangeException(string.Empty, "Inappropriate value, the value should be >= 0 and <= 20");
+            }
+        }
+
+        public Chainlink BodyArmor
+        {
+            get
+            {
+                return this.bodyArmor;
+            }
+            set
+            {
+                    this.bodyArmor = value;
+            }
+        }
+
+
+        public Axe Weapon
+        {
+            get
+            {
+                return this.weapon;
+            }
+            set
+            {
+                this.weapon = value;
+            }
+        }
 
         //Constructor
         public Warrior()
